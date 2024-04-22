63548482d6016c0ef09a32aca16bf0cf2f906ad6
Grant Davis <grantdavis303@gmail.com>
2024-04-22 09:50:16 -0600

    Set up iniitial seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+@lab_1 = Lab.create!(name: "Lab 1")
+@john = Scientist.create!(name: "John", specialty: "Chemical Reactions", university: "University of Nebraska", lab_id: @lab_1.id)
+@billy = Scientist.create!(name: "Billy", specialty: "Basketball IQ", university: "University of North Colrado", lab_id: @lab_3.id)
+
+@lab_2 = Lab.create!(name: "Lab 2")
+@grant = Scientist.create!(name: "Grant", specialty: "Chain Reactions", university: "University of Colorado Boulder", lab_id: @lab_2.id)
+@jared = Scientist.create!(name: "Jared", specialty: "Nuclear Fission", university: "Florida State University", lab_id: @lab_3.id)
+
+@teleportation = Experiment.create!(name: "Teleportation", objective: "This is the objective statement for the experiment", num_months: 20)
+@levitation = Experiment.create!(name: "Levitation", objective: "This is the objective statement for the experiment", num_months: 10)
+@time_travel = Experiment.create!(name: "Time Travel", objective: "This is the objective statement for the experiment", num_months: 6)
+@invisibility = Experiment.create!(name: "Invisibility", objective: "This is the objective statement for the experiment", num_months: 2)
+
+Scientist_experiment.create!(scientist_id: @john.id, experiment_id: @teleportation.id)
+Scientist_experiment.create!(scientist_id: @john.id, experiment_id: @levitation.id)
+Scientist_experiment.create!(scientist_id: @john.id, experiment_id: @invisibility.id)
+
+Scientist_experiment.create!(scientist_id: @billy.id, experiment_id: @teleportation.id)
+Scientist_experiment.create!(scientist_id: @billy.id, experiment_id: @invisibility.id)
+
+Scientist_experiment.create!(scientist_id: @grant.id, experiment_id: @teleportation.id)
+Scientist_experiment.create!(scientist_id: @grant.id, experiment_id: @invisibility.id)
+Scientist_experiment.create!(scientist_id: @grant.id, experiment_id: @time_travel.id)
+
+Scientist_experiment.create!(scientist_id: @jared.id, experiment_id: @invisibility.id)
+Scientist_experiment.create!(scientist_id: @jared.id, experiment_id: @time_travel.id)
