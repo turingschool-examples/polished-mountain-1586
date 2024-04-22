0cc266e80302c2c769288374ad799075fb7ce0f1
Mel Langhoff <mlanghoff@uwalumni.com>
2024-04-22 09:37:42 -0600

    Test:spec file complete for feat test

--- spec/features/scientists/show_spec.rb
+++ spec/features/scientists/show_spec.rb
@@ -5,28 +5,42 @@ Rspec.describe 'Scientist Show Pg' do
     @lab = Lab.create(name: 'Fermilab')
 
     @marie_curie = Scientist.create(name: 'Marie Curie', specialty: 'Radioactivity', university: 'University of Paris', lab: @lab)
-    @richard_feynman = Scientist.create(name: 'Richard Feynman', specialty: 'Quantum Electrodynamics', university: 'Princeton University', lab: @lab)
-    @isaac_newton = Scientist.create(name: 'Isaac Newton', specialty: 'Classical Mechanics', university: 'University of Cambridge', lab: @lab)
+    @richard_feynman = Scientist.create(name: 'Richard Feynman', specialty: 'Theoretical Physics', university: 'Princeton University', lab: @lab)
+    @isaac_newton = Scientist.create(name: 'Isaac Newton', specialty: 'Physics and Mathematics', university: 'University of Cambridge', lab: @lab)
 
-    @minerva = Experiment.create(name: 'MINERvA', objective: 'Study neutrino reactions with five different nuclei', num_months: 4, scientist: @marie_curie)
-    @double_helix = Experiment.create(name: 'Double Helix', objective: 'Study DNA structure', num_months: 6, scientist: @richard_feynman)
-    @laws_of_motion = Experiment.create(name: 'Laws of Motion', objective: 'Explore laws of motion and gravitation', num_months: 8, scientist: @isaac_newton)
+    @minerva = Experiment.create(name: 'MINERvA', objective: 'Study neutrino reactions with five different nuclei', num_months: 4)
+    @double_helix = Experiment.create(name: 'Double Helix', objective: 'Study DNA structure', num_months: 6)
+    @laws_of_motion = Experiment.create(name: 'Laws of Motion', objective: 'Explore laws of motion and gravitation', num_months: 8)
 
-    @scientist_experiment1 = ScientistExperiment.create(scientist: @marie_curie, experiment: @minerva)
-    @scientist_experiment2 = ScientistExperiment.create(scientist: @richard_feynman, experiment: @double_helix)
-    @scientist_experiment3 = ScientistExperiment.create(scientist: @isaac_newton, experiment: @laws_of_motion)
+    # MINERvA experiment
+    ScientistExperiment.create(scientist: @marie_curie, experiment: @minerva)
+    ScientistExperiment.create(scientist: @richard_feynman, experiment: @minerva)
 
+    # Double Helix experiment
+    ScientistExperiment.create(scientist: @richard_feynman, experiment: @double_helix)
+    ScientistExperiment.create(scientist: @isaac_newton, experiment: @double_helix)
+
+    # Laws of Motion experiment
+    ScientistExperiment.create(scientist: @isaac_newton, experiment: @laws_of_motion)
+    ScientistExperiment.create(scientist: @marie_curie, experiment: @laws_of_motion)
   end
 
-  it 'shows all attributes for the scientist' do
+  it 'shows all attributes and lab name for/associated w the scientist' do
+    visit 
 
-  end
-
-  it 'shows their university' do
-
+    @marie_curie = Scientist.create(name: 'Marie Curie', specialty: 'Radioactivity', university: 'University of Paris', lab: @lab)
+    within "#scientist-info" do
+      expect(page).to have_content("Scientist Name: Marie Curie")
+      expect(page).to have_content("Scientist Specialty: Radioactivity")
+      expect(page).to have_content("University: University of Paris")
+      expect(page).to have_content("Lab: Fermilab")
+    end
   end
 
   it 'shows all their experiments' do
-
+    within "#scientist-experiments" do
+      expect(page).to have_content(@laws_of_motion.name)
+      expect(page).to have_content(@minerva.name)
+    end
   end
 end
