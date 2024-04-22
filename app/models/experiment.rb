f5a25202b9662ea0092806a24e6672baefff35b2
NeilTheSeal <neil.hendren@gmail.com>
2024-04-22 10:02:45 -0600

    Implement scientist_experiment method

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,4 +1,8 @@
 class Experiment < ApplicationRecord
   has_many :scientist_experiments
   has_many :scientists, through: :scientist_experiments
+
+  def scientist_experiment(scientist)
+    scientist_experiments.where("scientist_id = ?", scientist.id)[0]
+  end
 end
