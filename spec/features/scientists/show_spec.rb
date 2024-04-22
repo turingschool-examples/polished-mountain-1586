12c058bb92a07e7bddf40d193e2b450b9d817cae
Mel Langhoff <mlanghoff@uwalumni.com>
2024-04-22 11:40:50 -0600

    User story 2 complete

--- spec/features/scientists/show_spec.rb
+++ spec/features/scientists/show_spec.rb
@@ -4,16 +4,14 @@ RSpec.describe "Scientist Show Page", type: :feature do
   before :each do
     @lab = Lab.create!(name: 'Cool Lab')
 
-    @marie_curie = Scientist.create!(name: 'Marie Curie', specialty: 'Radioactivity', university: 'University of Paris', lab: @lab)
-    @richard_feynman = Scientist.create!(name: 'Richard Feynman', specialty: 'Theoretical Physics', university: 'Princeton University', lab: @lab)
-    @isaac_newton = Scientist.create!(name: 'Isaac Newton', specialty: 'Physics and Mathematics', university: 'University of Cambridge', lab: @lab)
+    @marie_curie = Scientist.create!(name: 'Marie Curie', specialty: 'Radioactivity', university: 'University of Paris', lab: @lab, id: 1)
+    @richard_feynman = Scientist.create!(name: 'Richard Feynman', specialty: 'Theoretical Physics', university: 'Princeton University', lab: @lab, id: 2)
+    @isaac_newton = Scientist.create!(name: 'Isaac Newton', specialty: 'Physics and Mathematics', university: 'University of Cambridge', lab: @lab, id: 3)
 
-    # Create instance variables for experiments and associate scientists with experiments
-    @minerva = Experiment.create!(name: 'MINERvA', objective: 'Study neutrino reactions with five different nuclei', num_months: 4)
-    @double_helix = Experiment.create!(name: 'Double Helix', objective: 'Study DNA structure', num_months: 6)
-    @laws_of_motion = Experiment.create!(name: 'Laws of Motion', objective: 'Explore laws of motion and gravitation', num_months: 8)
+    @minerva = Experiment.create!(name: 'MINERvA', objective: 'Study neutrino reactions with five different nuclei', num_months: 4, id: 1)
+    @double_helix = Experiment.create!(name: 'Double Helix', objective: 'Study DNA structure', num_months: 10, id: 2)
+    @laws_of_motion = Experiment.create!(name: 'Laws of Motion', objective: 'Explore laws of motion and gravitation', num_months: 8, id: 3)
 
-    # Create ScientistExperiment records to associate scientists with experiments
     ScientistExperiment.create!(scientist: @marie_curie, experiment: @minerva)
     ScientistExperiment.create!(scientist: @richard_feynman, experiment: @minerva)
     ScientistExperiment.create!(scientist: @richard_feynman, experiment: @double_helix)
@@ -25,7 +23,6 @@ RSpec.describe "Scientist Show Page", type: :feature do
   it 'shows all attributes and lab name for the scientist' do
     visit scientist_path(@marie_curie)
 
-    # Verify scientist information is displayed
     within "#scientist-info" do
       expect(page).to have_content("Scientist Name: Marie Curie")
       expect(page).to have_content("Specialty: Radioactivity")
@@ -37,10 +34,27 @@ RSpec.describe "Scientist Show Page", type: :feature do
   it 'shows all their experiments' do
     visit scientist_path(@marie_curie)
 
-    # Verify experiments associated with the scientist are displayed
     within "#scientist-experiments" do
       expect(page).to have_content(@minerva.name)
       expect(page).to have_content(@laws_of_motion.name)
     end
+  end
+
+  it 'has a button to remove an experiment' do
+    visit scientist_path(@marie_curie)
+
+    expect(page).to have_button("Remove #{@minerva.name}")
+  end
+
+  it 'can redirect after deleting and experiment and show a scientists updated workload' do
+    visit scientist_path(@marie_curie)
+    expect(page).to have_content("MINERvA")
+    expect(page).to have_content("Laws of Motion")
+
+    click_on "Remove #{@minerva.name}"
+
+    expect(current_path).to eq(scientist_path(@marie_curie))
+    expect(page).to_not have_content("MINERvA")
+    expect(page).to have_content("Laws of Motion")
   end
 end
