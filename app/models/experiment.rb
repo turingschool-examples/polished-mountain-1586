d3a7398a1fac73641629cdfa2d072fa1ea1dce1b
NeilTheSeal <neil.hendren@gmail.com>
2024-04-22 10:24:37 -0600

    Write model method for long running experiments

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -5,4 +5,8 @@ class Experiment < ApplicationRecord
   def scientist_experiment(scientist)
     scientist_experiments.where("scientist_id = ?", scientist.id)[0]
   end
+
+  def self.long_running_experiments
+    where("experiments.num_months > 6").order("experiments.num_months DESC")
+  end
 end
