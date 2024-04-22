6956dc56d2e6523e8b4801afb52ca85c91441255
NeilTheSeal <neil.hendren@gmail.com>
2024-04-22 10:50:32 -0600

    Use rails route path names

--- app/controllers/scientist_experiments_controller.rb
+++ app/controllers/scientist_experiments_controller.rb
@@ -2,6 +2,6 @@ class ScientistExperimentsController < ApplicationController
   def destroy
     @scientist_experiment = ScientistExperiment.find(params[:id])
     @scientist_experiment.destroy
-    redirect_to("/scientists/#{@scientist_experiment.scientist_id}")
+    redirect_to(scientist_path(@scientist_experiment.scientist_id))
   end
 end
