c900326201e0fc3cfeb7a8e6cd7be292b87072c8
Grant Davis <grantdavis303@gmail.com>
2024-04-22 09:40:42 -0600

    Add Models and Model Tests

--- spec/models/lab_spec.rb
+++ spec/models/lab_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Lab do
-  it {should have_many :scientists}
+  it {should have_many(:scientists)}
+  it {should have_many(:experiments).through(:scientists)}
 end
